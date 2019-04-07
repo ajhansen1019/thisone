ccfdcf1e7c825e2298cd69d3e8bc2a4324ce2e86
ajhansen1019 <36546827+ajhansen1019@users.noreply.github.com>
2019-04-07 01:11:30 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var c;
+ 
+function setup() {
+ 	createCanvas(windowWidth, windowHeight);
+ 	c = color(255,255,255);
+	cursor('grab', [50], [50])
+}
+ 
+function mouseDragged() 
+{ 
+	strokeWeight(10);
+	stroke(c);
+	line(mouseX, mouseY, pmouseX, pmouseY);
+}
+ 
+function keyPressed()
+{
+	if(key == 'r' || key == 'R')
+	{
+		c = color(255, 0, 0);
+	}
+	
+	if(key == 'g' || key == 'G')
+	{
+		c = color(0, 255, 0);
+	}
+	
+	if(key == 'b' || key == 'B')
+	{
+		c = color(0, 0, 255);
+	}
+}
